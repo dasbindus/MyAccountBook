47f573a12c9ef1c1fd608df174dd959fc9a9a192
Bai Dong <bai123donggoog@163.com>
2015-05-10 15:54:27 +0800

    Baidong master commit. 添加RadioGroup的监听

--- src/jack/bai/studio/myaccount/ExpenditureFrag.java
+++ src/jack/bai/studio/myaccount/ExpenditureFrag.java
@@ -2,24 +2,32 @@ package jack.bai.studio.myaccount;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 public class ExpenditureFrag extends Fragment {
 
+	private final static String TAG = ExpenditureFrag.class.getSimpleName();
+
 	private Spinner mSpinner;
+	private RadioGroup mRadioGroup;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.expenditure_frag, container,
-				false);
+		final View view = inflater.inflate(R.layout.expenditure_frag,
+				container, false);
+
 		mSpinner = (Spinner) view.findViewById(R.id.spinner1);
 		String[] mSpinnerItems = getResources().getStringArray(
 				R.array.spinnername_expend);
@@ -32,7 +40,7 @@ public class ExpenditureFrag extends Fragment {
 		mSpinner.setOnItemSelectedListener(new OnItemSelectedListener() {
 
 			@Override
-			public void onItemSelected(AdapterView<?> arg0, View v,
+			public void onItemSelected(AdapterView<?> parent, View v,
 					int position, long id) {
 				// TODO Auto-generated method stub
 				Toast.makeText(getActivity(), arrayAdapter.getItem(position),
@@ -40,10 +48,24 @@ public class ExpenditureFrag extends Fragment {
 			}
 
 			@Override
-			public void onNothingSelected(AdapterView<?> arg0) {
+			public void onNothingSelected(AdapterView<?> parent) {
 				// TODO Auto-generated method stub
 				Toast.makeText(getActivity(), "请选择类别！", Toast.LENGTH_SHORT)
 						.show();
+			}
+		});
+
+		mRadioGroup = (RadioGroup) view.findViewById(R.id.radioGroup1);
+		mRadioGroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(RadioGroup parent, int position) {
+				// 获取选中项的ID
+				int radioButtonId = parent.getCheckedRadioButtonId();
+				// 根据ID获取RadioButton的实例
+				RadioButton rb = (RadioButton) view.findViewById(radioButtonId);
+				// 获取选中项的内容
+				Log.i(TAG, "选中的RadioButton是：" + rb.getText());
 			}
 		});
 		return view;
